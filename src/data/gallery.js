065d1efa657502a80759c80a688bf9221be09e4d
FullStackDev <whitehorse9021@gmail.com>
2023-04-22 04:17:17 -0500

    Code refactoring

---
+++ src/data/gallery.js
@@ -0,0 +1,17 @@
+import ape1 from "../../images/about/ape01.svg";
+import ape2 from "../../images/about/ape02.svg";
+import ape3 from "../../images/about/ape03.svg";
+import ape4 from "../../images/about/ape04.svg";
+
+import gal1 from "../../images/gallery/gal01.svg";
+import gal2 from "../../images/gallery/gal02.svg";
+import gal3 from "../../images/gallery/gal03.svg";
+import gal4 from "../../images/gallery/gal04.svg";
+import gal5 from "../../images/gallery/gal05.svg";
+import gal6 from "../../images/gallery/gal06.svg";
+
+const apes = [ape1, ape2, ape3, ape4];
+
+const galleries = [gal1, gal2, gal3, gal4, gal5, gal6];
+
+export { apes, galleries };
--- src/data/index.js
+++ src/data/index.js
@@ -1,9 +1,12 @@
-
-export { default as faqQuestions } from './faq.js';
-export { default as meetApes } from './meetapes.js';
-export { alphaTeam, fusionTeam } from './team.js';
-export { collectionFilterOptions, myCollectionOptions } from './selectOptions.js';
-export { default as fusionCollections } from './fusionCollections.js';
-export { default as alphaCollections } from './alphaCollections.js';
-export { default as myCollections } from './collections.js';
-export { default as filterOptions } from './filter.js';
+export { default as faqQuestions } from "./faq.js";
+export { default as meetApes } from "./meetapes.js";
+export { alphaTeam, fusionTeam } from "./team.js";
+export {
+  collectionFilterOptions,
+  myCollectionOptions,
+} from "./selectOptions.js";
+export { default as fusionCollections } from "./fusionCollections.js";
+export { default as alphaCollections } from "./alphaCollections.js";
+export { default as myCollections } from "./collections.js";
+export { default as filterOptions } from "./filter.js";
+export { apes, galleries } from "./gallery.js";
